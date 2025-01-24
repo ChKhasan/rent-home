ba0f79ce8f622677bd46283d0c32f7c168a8903b
Khasan Chulliyev <x.chulliyev@unicon.uz>
2025-01-24 15:25:29 +0500

    fix: all 2

--- src/app/core/common/form-control.ts
+++ src/app/core/common/form-control.ts
@@ -9,4 +9,4 @@ export const addressControl = new FormControl('', [Validators.required, Validato
 export const passwordControl = new FormControl(undefined, [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*\d).*$/)]);
 export const passwordRegisterControl = new FormControl(undefined, [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*\d).*$/)]);
 export const numberControl = new FormControl('', [Validators.required, Validators.pattern(/^\d{2} \d{3} \d{2} \d{2}$/)]);
-export const emailControl = new FormControl('', [Validators.required, Validators.email]);
+export const emailControl = new FormControl('', [Validators.email]);
--- src/app/pages/profile/profile/profile.component.ts
+++ src/app/pages/profile/profile/profile.component.ts
@@ -24,7 +24,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [TabComponent, RouterLink, SelectButtonModule, InputMaskModule, InputNumberModule, InputTextModule, InvaidTextComponent, NgIf, PaginatorModule, ReactiveFormsModule, NgClass, ButtonModule, FileUploadModule, LikesComponent, AnnouncementsComponent],
+  imports: [RouterLink, SelectButtonModule, InputMaskModule, InputNumberModule, InputTextModule, InvaidTextComponent, NgIf, PaginatorModule, ReactiveFormsModule, NgClass, ButtonModule, FileUploadModule, LikesComponent, AnnouncementsComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
